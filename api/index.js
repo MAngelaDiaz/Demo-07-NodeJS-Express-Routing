e8aa850f4366933d6ff55fb49648ee7f7e31b002
MAngelaDiaz <125098637+MAngelaDiaz@users.noreply.github.com>
2024-11-12 04:17:37 +0000

    response edited

--- api/index.js
+++ api/index.js
@@ -11,7 +11,7 @@ app.listen(PORT, () => {
 
 //Home Page
 app.get('/', (request, response) => {
-  response.response.send('<h1>API Homepage</h1>');
+  response.send('<h1>API Homepage</h1>');
 });
 
 //Customers
